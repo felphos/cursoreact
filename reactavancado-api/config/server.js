dc2a8701659c1eefad8787340b2721d237366003
felphos <felphos@gmail.com>
2022-02-06 16:36:42 +0100

    Before cloning landing page

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '093d67416aefe0eee6926d801d953742'),
+    },
+  },
+});
